a4b7b764df1f714358d5b28106bb5022b5c8d4c3
sedawk <sedawk@naver.com>
2019-07-12 21:01:19 +0900

    Adds a solution for car and cdr functions

---
+++ CarAndCdr/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import unittest
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(func):
+    def first(a, b):
+        return a
+    return func(first)
+
+def cdr(func):
+    def second(a, b):
+        return b
+    return func(second)
+
+
+class TestCase(unittest.TestCase):
+    def testA(self):
+        print cons(3, 4)
+        assert 3 == car(cons(3, 4))
+
+    def testB(self):
+        assert 4 == cdr(cons(3, 4))
+
+if __name__ == "__main__":
+    unittest.main()
+    
